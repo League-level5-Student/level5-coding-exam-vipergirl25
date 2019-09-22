7fe7222ff0e68fe57202eccda90a6bb7b267eef9
league <league@imac-31.attlocal.net>
2019-09-22 11:07:05 -0700

    completed test

--- src/Coding_Exam_A/CodingExamA.java
+++ src/Coding_Exam_A/CodingExamA.java
@@ -10,11 +10,15 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class CodingExamA implements draw2 {
 	int howMany;
-	static Color color;
+	static Color customColor;
 	static int sides;
 	public static void main(String[] args) {
 		String howManyString = JOptionPane.showInputDialog("how many robots?"); int howMany = Integer.parseInt(howManyString);
-		String colorString = JOptionPane.showInputDialog("what color?");  color = Color.getColor(colorString);
+		
+		String color = JOptionPane.showInputDialog("enter a r value: "); int colorInt = Integer.parseInt(color);
+		String color1 = JOptionPane.showInputDialog("enter a r value: ");int colorInt1 = Integer.parseInt(color1);
+		String color2 = JOptionPane.showInputDialog("enter a r value: ");int colorInt2 = Integer.parseInt(color2);
+		customColor = new Color(colorInt, colorInt1, colorInt2);
 		String sidesString = JOptionPane.showInputDialog("how many sides?");  sides = Integer.parseInt(sidesString);
 		/*
 		 * Write a program that asks the user for three pieces of information.
@@ -31,14 +35,20 @@ public class CodingExamA implements draw2 {
 		 */
 		CodingExamA a = new CodingExamA();
 		ArrayList<Thread>threads = new ArrayList<Thread>();
-		System.out.println(howMany);
-		for (int i = 1; i < howMany+1; i++) {
+		
+
+		for (int i = 0; i < howMany; i++) {
 			int hi = 100*i;
+			System.out.println(i);
+			
 			Thread ra = new Thread(()->{
-				
 				Robot r = new Robot();
+				r.penDown();
 				r.moveTo(50, 50);
+				
 				r.moveTo(hi+100, 100);
+				
+				r.penDown();
 				a.draw(r);
 			});
 			threads.add(ra);
@@ -58,8 +68,8 @@ public class CodingExamA implements draw2 {
 	@Override
 	public void draw(Robot r) {
 		r.setSpeed(100);
-		r.penDown();
-		r.setPenColor(color);
+		System.out.println(customColor);
+		r.setPenColor(customColor);
 		for (int j = 0; j < sides; j++) {
 			r.move(100);r.turn(360/sides);
 			
--- src/Coding_Exam_B/CodingExamB.java
+++ src/Coding_Exam_B/CodingExamB.java
@@ -1,12 +1,14 @@
 package Coding_Exam_B;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class CodingExamB {
+	static String finalString = "";
 	/*
 	 * This is a logging program for collecting TODO comments in a program. The program will scan
 	 * Through all the files in the Coding_Exam_B/classes package, and collect all the comments that start
@@ -15,7 +17,37 @@ public class CodingExamB {
 	 */
 	
 	
-	public static String getLoggingInfo(String fileName) {
+	public static String getLoggingInfo(String fileName) throws IOException {
+
+		try {
+		
+			
+			BufferedReader br = new BufferedReader(new FileReader(fileName));
+			String line = br.readLine();
+			int lineNumber = 0;
+		
+		
+			while(line!=null) {
+				if(line.contains("//TODO")) {
+					finalString+=fileName + " line " + lineNumber + "\n" + line + "\n";
+				}
+				line = br.readLine();
+				lineNumber++;
+			}
+		
+	
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	
+
+			
+		
+		
 		/*
 		 * 1. Complete the getLoggingInfoMethod.
 		 *    The method takes in a String for a file and returns a String. 
@@ -25,10 +57,11 @@ public class CodingExamB {
 		 *    the line number for where each TODO was found. 
 		*/
 		
-		return "";
+		
+		return finalString;
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		String finalLogString = getLoggingInfo("src/Coding_Exam_B/classes/Camera.java");
 		finalLogString += getLoggingInfo("src/Coding_Exam_B/classes/RayTracedImageViewer.java");
 		finalLogString += getLoggingInfo("src/Coding_Exam_B/classes/RayTracer.java");
@@ -37,6 +70,15 @@ public class CodingExamB {
 		/*
 		 * 2. Write the finalLogString to a file called TODO_Log.txt. The file should match TODO_Log_example.txt. 
 		 */
+		try {
+			System.out.println(finalString);
+			FileWriter fw = new FileWriter("src/Coding_Exam_B/TODO_Log.txt");
+			fw.write(finalString);
+			System.out.println(finalString);
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 }
